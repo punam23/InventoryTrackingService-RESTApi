dc973ad2f57f0576468bdc4840026db027a147d9
KUMARI PUNAM <kumaripunam23@gmail.com>
2023-03-21 00:05:39 +0530

    Add files via upload

---
+++ InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryTracking.Models
+{
+   
+        public class InventoryItem
+        {
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public string CreatedOn { get; set; }
+        }
+    
+}
